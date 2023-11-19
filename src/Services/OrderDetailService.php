e1b4d758d8105de3d6f63a12fabafdf0d59e1a78
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-11-20 03:30:08 +0600

    LP-148 wait for new pull request

--- src/Services/OrderDetailService.php
+++ src/Services/OrderDetailService.php
@@ -2,6 +2,7 @@
 
 namespace Fintech\Transaction\Services;
 
+use Fintech\Business\Facades\Business;
 use Fintech\Transaction\Interfaces\OrderDetailRepository;
 
 /**
@@ -59,5 +60,37 @@ class OrderDetailService
     public function import(array $filters)
     {
         return $this->orderDetailRepository->create($filters);
+    }
+
+    public function masterUserTransaction(array $data)
+    {
+        dd($data);
+    }
+    public function masterUserRefundTransaction(array $data)
+    {
+        dd($data);
+    }
+    public function userTransaction($data)
+    {
+        $data->role_id = $data->user->roles[0]->getKey();
+        $serviceStateData['role_id'] = $data->role_id;
+        $serviceStateData['service_id'] = $data->service_id;
+        $serviceStateData['source_country_id'] = $data->source_country_id;
+        $serviceStateData['destination_country_id'] = $data->destination_country_id;
+       // print_r($serviceStateData);exit();
+        $serviceState = Business::serviceStat()->list($serviceStateData)->first()->toArray();
+        //$serviceState['id'] = $serviceState['id'];
+        $charge_break_down = Business::chargeBreakDown(['service_state_id' => $serviceState['id']])->list();
+        /*$serviceState = json_decode($serviceState['service_state_data'])[0];
+        $serviceStateJsonData['charge'] = $serviceState->charge;
+        $serviceStateJsonData['discount'] = $serviceState->charge;
+        $serviceStateJsonData['commission'] = $serviceState->charge;*/
+        //$charge_break_down = Business::chargeBreakDown()->list();
+        print_r($charge_break_down);exit();
+    }
+
+    public function userRefundTransaction(array $data)
+    {
+        dd($data);
     }
 }
