61c19f182b21a2ad8edf4f3400710790c92e28b7
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-11-21 02:16:47 +0600

    LP-148 order details work was continue

--- src/Services/OrderDetailService.php
+++ src/Services/OrderDetailService.php
@@ -3,6 +3,8 @@
 namespace Fintech\Transaction\Services;
 
 use Fintech\Business\Facades\Business;
+use Fintech\Core\Enums\Transaction\OrderStatusConfig;
+use Fintech\Transaction\Facades\Transaction;
 use Fintech\Transaction\Interfaces\OrderDetailRepository;
 
 /**
@@ -77,20 +79,112 @@ class OrderDetailService
         $serviceStateData['service_id'] = $data->service_id;
         $serviceStateData['source_country_id'] = $data->source_country_id;
         $serviceStateData['destination_country_id'] = $data->destination_country_id;
-       // print_r($serviceStateData);exit();
+        $serviceStateData['amount'] = $data->amount;
+        $serviceStateData['enable'] = true;
         $serviceState = Business::serviceStat()->list($serviceStateData)->first()->toArray();
-        //$serviceState['id'] = $serviceState['id'];
-        $charge_break_down = Business::chargeBreakDown(['service_state_id' => $serviceState['id']])->list();
-        /*$serviceState = json_decode($serviceState['service_state_data'])[0];
-        $serviceStateJsonData['charge'] = $serviceState->charge;
-        $serviceStateJsonData['discount'] = $serviceState->charge;
-        $serviceStateJsonData['commission'] = $serviceState->charge;*/
-        //$charge_break_down = Business::chargeBreakDown()->list();
-        print_r($charge_break_down);exit();
+
+        $serviceStateData['service_stat_id'] = $serviceState['id'];
+        $charge_break_down = Business::chargeBreakDown()->list($serviceStateData)->first();
+        $serviceState = $serviceState['service_stat_data'][0];
+        if($charge_break_down){
+            $serviceStateJsonData['charge'] = $charge_break_down->charge_break_down_charge;
+            $serviceStateJsonData['discount'] = $charge_break_down->charge_break_down_discount;
+            $serviceStateJsonData['commission'] = $charge_break_down->charge_break_down_commission;
+            $serviceStateJsonData['charge_break_down'] = $charge_break_down->getKey();
+        }else{
+            $serviceStateJsonData['charge'] = $serviceState['charge'];
+            $serviceStateJsonData['discount'] = $serviceState['discount'];
+            $serviceStateJsonData['commission'] = $serviceState['commission'];
+            $serviceStateJsonData['service_stat_id'] = $serviceStateData['service_stat_id'];
+        }
+
+        //TODO
+        $charge_amount = 0;
+        $discount_amount = 0;
+        $commission_amount = 0;
+
+
+        $data->order_detail_cause_name = "cash_deposit";
+        $orderDetailStore = Transaction::orderDetail()->create(self::orderDetailsDataArrange($data));
+        $data->amount = - $charge_amount;
+        $data->converted_amount = - $charge_amount;
+        $data->order_detail_cause_name = "charge";
+        //$data->notes = "charge";
+        $data->step = 2;
+        $data->order_detail_parent_id = $orderDetailStore->getKey();
+        $orderDetailStoreForCharge = Transaction::orderDetail()->create(self::orderDetailsDataArrange($data));
+        $data->amount = - $discount_amount;
+        $data->converted_amount = - $discount_amount;
+        $data->order_detail_cause_name = "discount";
+        //$data->notes = "charge";
+        $data->step = 3;
+        $data->order_detail_parent_id = $orderDetailStore->getKey();
+        $updateData['order_data']['previous_amount'] = 0;
+        $orderDetailStoreForDiscount = Transaction::orderDetail()->create(self::orderDetailsDataArrange($data));
+        print_r($orderDetailStoreForDiscount);exit();
     }
 
     public function userRefundTransaction(array $data)
     {
         dd($data);
+    }
+
+    public function chargeStore($data)
+    {
+        $orderData['order_detail_amount'] = $data['commission_amount'];
+        $orderData['converted_amount'] = $data['commission_amount'];
+        $orderData['order_detail_cause_name'] = 'charge';
+        $orderData['order_detail_parent_id'] = 0;
+        $orderData['step'] = 0;
+        dd($data);
+    }
+    public function discountStore($data)
+    {
+        $orderData['order_detail_amount'] = $data['commission_amount'];
+        $orderData['converted_amount'] = $data['commission_amount'];
+        $orderData['order_detail_cause_name'] = 'discount';
+        $orderData['order_detail_parent_id'] = 0;
+        $orderData['step'] = 0;
+        dd($data);
+    }
+    public function commissionStore($data)
+    {
+        $orderData['order_detail_amount'] = $data['commission_amount'];
+        $orderData['converted_amount'] = $data['commission_amount'];
+        $orderData['order_detail_cause_name'] = 'commission';
+        $orderData['order_detail_parent_id'] = 0;
+        $orderData['step'] = 0;
+        dd($data);
+    }
+
+    /**
+     * @param $data
+     * @return array
+     */
+    private function orderDetailsDataArrange($data): array
+    {
+        $orderData['order_id'] = $data->getKey();
+        $orderData['source_country_id'] = $data->source_country_id;
+        $orderData['destination_country_id'] = $data->destination_country_id;
+        $orderData['sender_receiver_id'] = $data->sender_receiver_id;
+        $orderData['user_id'] = $data->user_id;
+        $orderData['service_id'] = $data->service_id;
+        $orderData['transaction_form_id'] = $data->transaction_form_id;
+        $orderData['order_detail_date'] = $data->ordered_at;
+        $orderData['order_detail_cause_name'] = $data->order_detail_cause_name;
+        $orderData['order_detail_amount'] = $data->amount;
+        $orderData['order_detail_currency'] = $data->currency;
+        $orderData['converted_amount'] = $data->converted_amount;
+        $orderData['converted_currency'] = $data->converted_currency;
+        $orderData['order_detail_number'] = 0;
+        $orderData['order_detail_response_id'] = $data->order_number;
+        $orderData['step'] = 1;
+        $orderData['risk'] = $data->risk;
+        $orderData['notes'] = 0;
+        $orderData['is_refundable'] = $data->is_refundable;
+        $orderData['order_detail_data'] = $data->order_data;
+        $orderData['status'] = 'success';
+        return $orderData;
+
     }
 }
