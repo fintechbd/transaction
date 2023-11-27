3aaf5a0ffacd502df7c596497652e3d8226712f3
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-11-28 03:44:03 +0600

    LP-151 remit delay check add

--- src/Services/OrderService.php
+++ src/Services/OrderService.php
@@ -4,6 +4,7 @@ namespace Fintech\Transaction\Services;
 
 use Carbon\Carbon;
 use Fintech\Business\Facades\Business;
+use Fintech\MetaData\Facades\MetaData;
 use Fintech\Transaction\Facades\Transaction;
 use Fintech\Transaction\Interfaces\OrderRepository;
 
@@ -63,7 +64,11 @@ class OrderService
         return $this->orderRepository->create($filters);
     }
 
-    public function transactionDelayCheck($data)
+    /**
+     * @param $data
+     * @return array
+     */
+    public function transactionDelayCheck($data): array
     {
         $delayCheck = config('fintech.transaction.delay_time');
         if($delayCheck > 0){
@@ -77,17 +82,32 @@ class OrderService
             $input['created_at_start_date_time'] = $created_at;
             $input['created_at_end_date_time'] = Carbon::now()->format('Y-m-d H:i:s');
             $input['service_delay'] = 'yes';
-            $service_type_parent_id = Business::service()->find($input['service_id']);
-            if((isset($service_type_parent_id->serviceType->service_type_slug)?$service_type_parent_id->serviceType->service_type_slug:null) == 'fund_deposit'){
-                $input['status'] = array('processing');
-                unset($input['created_at_start_date_time'], $input['created_at_end_date_time']);
-            }
-            $orderCheck = Transaction::order()->list($input);
-            if($orderCheck->first()) {
-                $returnValue['countValue'] = $orderCheck->count();
-                $remainingTime = strtotime($created_at) - strtotime($orderCheck->first()->order_at);
-                $returnValue['remainingTime'] = $delayCheck - (int)($remainingTime / 60);
-                $returnValue['delayTime'] = $delayCheck;
+            $service_type_parent = Business::service()->list(['service_id' => $input['service_id'], 'service_delay' => $input['service_delay']])->first();
+            if($service_type_parent){
+                if(isset($data['order_data']['account_number']) && $data['order_data']['account_number'] != null) {
+                    if ((isset($service_type_parent->serviceType->service_type_slug) ? $service_type_parent->serviceType->service_type_slug : null) == 'wallet_transfer') {
+                        $country = MetaData::country()->list(['country_id'=>$input['destination_country_id']])->first();
+                        $input['account_number'] = $country->phone_code + $data['order_data']['account_number'];
+                    }else {
+                        $input['account_number'] = $data['order_data']['account_number'];
+                        $input['bank_id'] = isset($data['order_data']['bank_id']) ? $data['order_data']['bank_id'] : null;
+                        $input['bank_branch_id'] = isset($data['order_data']['bank_branch_id']) ? $data['order_data']['bank_branch_id'] : null;
+                    }
+                }elseif((isset($service_type_parent->serviceType->service_type_slug)?$service_type_parent->serviceType->service_type_slug:null) == 'fund_deposit'){
+                    $input['status'] = array('processing');
+                    unset($input['created_at_start_date_time'], $input['created_at_end_date_time']);
+                }
+                $orderCheck = Transaction::order()->list($input);
+                if($orderCheck->first()) {
+                    $returnValue['countValue'] = $orderCheck->count();
+                    $remainingTime = strtotime($created_at) - strtotime($orderCheck->first()->order_at);
+                    $returnValue['remainingTime'] = $delayCheck - (int)($remainingTime / 60);
+                    $returnValue['delayTime'] = $delayCheck;
+                }else {
+                    $returnValue['countValue'] = 0;
+                    $returnValue['remainingTime'] = 0;
+                    $returnValue['delayTime'] = 0;
+                }
             }else {
                 $returnValue['countValue'] = 0;
                 $returnValue['remainingTime'] = 0;
