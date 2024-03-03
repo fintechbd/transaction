d8329011ea125e9883427ed4fddc46555a5c407d
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-03-03 16:00:07 +0600

    typo fixed

--- src/Services/OrderQueueService.php
+++ src/Services/OrderQueueService.php
@@ -19,12 +19,12 @@ class OrderQueueService
     {
     }
 
-    public function find($id, bool $onlyTrashed = false): Model|Model|null
+    public function find($id, bool $onlyTrashed = false): Model|null
     {
         return $this->orderQueueRepository->find($id, $onlyTrashed);
     }
 
-    public function update($id, array $inputs = []): Model|Model|null
+    public function update($id, array $inputs = []): Model|null
     {
         return $this->orderQueueRepository->update($id, $inputs);
     }
@@ -50,12 +50,12 @@ class OrderQueueService
 
     }
 
-    public function import(array $filters): Model|Model|null
+    public function import(array $filters): Model|null
     {
         return $this->orderQueueRepository->create($filters);
     }
 
-    public function create(array $inputs = []): Model|Model|null
+    public function create(array $inputs = []): Model|null
     {
         return $this->orderQueueRepository->create($inputs);
     }
