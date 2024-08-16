616b560a23e36564d24e93e2f93097c61b7cb544
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-08-17 02:14:54 +0600

    assignment columns added

---
+++ database/migrations/2024_07_16_175448_update_assign_colum_in_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if(!Schema::hasColumn('orders', 'assigned_user_id')) {
+            Schema::table('orders', function (Blueprint $table) {
+                $table->foreignId('assigned_user_id')->nullable()->after('status');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        if(Schema::hasColumn('orders', 'assigned_user_id')) {
+            Schema::table('orders', function (Blueprint $table) {
+                $table->dropColumn('assigned_user_id');
+            });
+        }
+    }
+};
---
+++ database/migrations/2024_08_16_201019_update_service_vendor_id_column_in_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->foreignId('service_vendor_id')->nullable()->after('service_id');
+            $table->json('timeline')->nullable()->after('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('service_vendor_id');
+            $table->dropColumn('timeline');
+        });
+    }
+};
