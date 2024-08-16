ba6dac3a68bfc1baea0030d5b4c2f269d4356d29
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-08-17 03:04:59 +0600

    vendor loader added

--- database/migrations/2024_08_16_201019_update_service_vendor_id_column_in_orders_table.php
+++ database/migrations/2024_08_16_201019_update_service_vendor_id_column_in_orders_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 return new class extends Migration
 {
@@ -12,8 +13,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->foreignId('service_vendor_id')->default(config('fintech.business.default_vendor', 1))->after('service_id');
-            $table->json('timeline')->nullable()->after('status');
+            $table->foreignId('service_vendor_id')
+                ->default(config('fintech.business.default_vendor', 1))
+                ->after('service_id');
+            $table->string('vendor')
+                ->default(Str::slug(config('fintech.business.default_vendor_name'), '_'))
+                ->after('service_vendor_id');
+            $table->json('timeline')
+                ->nullable()
+                ->after('status');
         });
     }
 
@@ -25,6 +33,7 @@ return new class extends Migration
         Schema::table('orders', function (Blueprint $table) {
             $table->dropColumn('service_vendor_id');
             $table->dropColumn('timeline');
+            $table->dropColumn('vendor');
         });
     }
 };
