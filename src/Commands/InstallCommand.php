341464fff58c9b487e19076806be38f48d299033
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-08-31 15:47:45 +0600

    project setup updated

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -2,18 +2,49 @@
 
 namespace Fintech\Transaction\Commands;
 
+use Fintech\Core\Traits\HasCoreSettingTrait;
 use Illuminate\Console\Command;
 
 class InstallCommand extends Command
 {
     public $signature = 'transaction:install';
 
-    public $description = 'My command';
+    use HasCoreSettingTrait;
+
+    public $description = 'Configure the system for the `fintech/transaction` module';
+
+    private array $settings = [
+        [
+            'package' => 'transaction',
+            'label' => 'Transaction Delay Time',
+            'description' => 'Transaction Delay Time',
+            'key' => 'delay_time',
+            'type' => 'integer',
+            'value' => '15'
+        ],
+        [
+            'package' => 'transaction',
+            'label' => 'Transaction Minimum Balance',
+            'description' => 'Transaction Minimum Balance',
+            'key' => 'minimum_balance',
+            'type' => 'float',
+            'value' => '0.00'
+        ],
+    ];
 
     public function handle(): int
     {
-        $this->comment('All done');
+        try {
 
-        return self::SUCCESS;
+            $this->addOverwriteSetting();
+
+            return self::SUCCESS;
+
+        } catch (\Exception $e) {
+
+            $this->components->twoColumnDetail($e->getMessage(), '<fg=red;options=bold>ERROR</>');
+
+            return self::FAILURE;
+        }
     }
 }
