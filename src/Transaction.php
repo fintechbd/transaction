6103f0711b0fdf37e1fbe07a528b5bf903a5010e
hafijul233 <hafijul233@users.noreply.github.com>
2024-09-19 20:16:39 +0000

    Fix styling

--- src/Transaction.php
+++ src/Transaction.php
@@ -20,88 +20,88 @@ class Transaction
      * @return TransactionFormService
      */
     public function transactionForm($filters = null)
-{
-	return \singleton(TransactionFormService::class, $filters);
+    {
+        return \singleton(TransactionFormService::class, $filters);
     }
 
     /**
      * @return OrderService
      */
     public function order($filters = null)
-{
-	return \singleton(OrderService::class, $filters);
+    {
+        return \singleton(OrderService::class, $filters);
     }
 
     /**
      * @return OrderDetailService
      */
     public function orderDetail($filters = null)
-{
-	return \singleton(OrderDetailService::class, $filters);
+    {
+        return \singleton(OrderDetailService::class, $filters);
     }
 
     /**
      * @return ChartClassService
      */
     public function chartClass($filters = null)
-{
-	return \singleton(ChartClassService::class, $filters);
+    {
+        return \singleton(ChartClassService::class, $filters);
     }
 
     /**
      * @return ChartTypeService
      */
     public function chartType($filters = null)
-{
-	return \singleton(ChartTypeService::class, $filters);
+    {
+        return \singleton(ChartTypeService::class, $filters);
     }
 
     /**
      * @return ChartEntryService
      */
     public function chartEntry($filters = null)
-{
-	return \singleton(ChartEntryService::class, $filters);
+    {
+        return \singleton(ChartEntryService::class, $filters);
     }
 
     /**
      * @return UserAccountService
      */
     public function userAccount($filters = null)
-{
-	return \singleton(UserAccountService::class, $filters);
+    {
+        return \singleton(UserAccountService::class, $filters);
     }
 
     /**
      * @return OrderQueueService
      */
     public function orderQueue($filters = null)
-{
-	return \singleton(OrderQueueService::class, $filters);
+    {
+        return \singleton(OrderQueueService::class, $filters);
     }
 
     /**
      * @return ManualRefundService
      */
     public function manualRefund($filters = null)
-{
-	return \singleton(ManualRefundService::class, $filters);
+    {
+        return \singleton(ManualRefundService::class, $filters);
     }
 
     /**
      * @return RewardPointService
      */
     public function rewardPoint($filters = null)
-{
-	return \singleton(RewardPointService::class, $filters);
+    {
+        return \singleton(RewardPointService::class, $filters);
     }
 
     /**
      * @return RedeemPointService
      */
     public function redeemPoint($filters = null)
-{
-	return \singleton(RedeemPointService::class, $filters);
+    {
+        return \singleton(RedeemPointService::class, $filters);
     }
 
     //** Crud Service Method Point Do not Remove **//
