f8861989b048f31a120a6cee85178a345e9f0d43
hafijul233 <hafijul233@users.noreply.github.com>
2024-09-29 17:56:34 +0000

    Fix styling

--- src/Repositories/Eloquent/OrderDetailRepository.php
+++ src/Repositories/Eloquent/OrderDetailRepository.php
@@ -29,7 +29,7 @@ class OrderDetailRepository extends EloquentRepository implements InterfacesOrde
         $query = $this->model->newQuery();
 
         //Searching
-        if (!empty($filters['search'])) {
+        if (! empty($filters['search'])) {
             $query->where(function ($query) use ($filters) {
                 $query->where($this->model->getKeyName(), 'like', "%{$filters['search']}%")
                     ->orWhere('order_detail_number', 'like', "%{$filters['search']}%")
@@ -38,51 +38,51 @@ class OrderDetailRepository extends EloquentRepository implements InterfacesOrde
             });
         }
 
-        if (!empty($filters['id_not_in'])) {
-            $query->whereNotIn($this->model->getKeyName(), (array)$filters['id_not_in']);
+        if (! empty($filters['id_not_in'])) {
+            $query->whereNotIn($this->model->getKeyName(), (array) $filters['id_not_in']);
         }
 
-        if (!empty($filters['id_in'])) {
-            $query->whereIn($this->model->getKeyName(), (array)$filters['id_in']);
+        if (! empty($filters['id_in'])) {
+            $query->whereIn($this->model->getKeyName(), (array) $filters['id_in']);
         }
 
-        if (!empty($filters['order_id'])) {
+        if (! empty($filters['order_id'])) {
             $query->where('order_id', '=', $filters['order_id']);
         }
 
-        if (!empty($filters['source_country_id'])) {
+        if (! empty($filters['source_country_id'])) {
             $query->where('source_country_id', '=', $filters['source_country_id']);
         }
 
-        if (!empty($filters['destination_country_id'])) {
+        if (! empty($filters['destination_country_id'])) {
             $query->where('destination_country_id', '=', $filters['destination_country_id']);
         }
 
-        if (!empty($filters['order_detail_parent_id'])) {
+        if (! empty($filters['order_detail_parent_id'])) {
             $query->where('order_detail_parent_id', '=', $filters['order_detail_parent_id']);
         }
 
-        if (!empty($filters['sender_receiver_id'])) {
+        if (! empty($filters['sender_receiver_id'])) {
             $query->where('sender_receiver_id', '=', $filters['sender_receiver_id']);
         }
 
-        if (!empty($filters['user_id'])) {
+        if (! empty($filters['user_id'])) {
             $query->where('user_id', '=', $filters['user_id']);
         }
 
-        if (!empty($filters['service_id'])) {
+        if (! empty($filters['service_id'])) {
             $query->where('service_id', '=', $filters['service_id']);
         }
 
-        if (!empty($filters['transaction_form_id'])) {
+        if (! empty($filters['transaction_form_id'])) {
             $query->where('transaction_form_id', '=', $filters['transaction_form_id']);
         }
 
-        if (!empty($filters['order_detail_currency'])) {
+        if (! empty($filters['order_detail_currency'])) {
             $query->where('order_detail_currency', '=', $filters['order_detail_currency']);
         }
 
-        if (!empty($filters['converted_currency'])) {
+        if (! empty($filters['converted_currency'])) {
             $query->where('converted_currency', '=', $filters['converted_currency']);
         }
 
