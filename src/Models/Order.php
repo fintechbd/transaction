dcf880a348db7b6e589d8d9b391ee5411b470897
hafijul233 <hafijul233@users.noreply.github.com>
2024-10-17 11:13:37 +0000

    Fix styling

--- src/Models/Order.php
+++ src/Models/Order.php
@@ -7,7 +7,6 @@ use Fintech\Business\Models\ServiceVendor;
 use Fintech\Core\Abstracts\BaseModel;
 use Fintech\Core\Enums\Auth\RiskProfile;
 use Fintech\Core\Enums\Transaction\OrderStatus;
-use Fintech\Core\Traits\AuditableTrait;
 use Fintech\Core\Traits\BlameableTrait;
 use Fintech\Transaction\Traits\AuthRelations;
 use Fintech\Transaction\Traits\BusinessRelations;
