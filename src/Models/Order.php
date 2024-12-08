31af62cca7e3f1221c0a2cc3c5e3710c42f42046
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-12-08 12:53:04 +0600

    optimized importing

--- src/Models/Order.php
+++ src/Models/Order.php
@@ -17,6 +17,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @property \Fintech\Core\Enums\Reload\DepositStatus $status
+ * @property float $previous_balance
+ * @property float $current_balance
+ * @property float $transaction_amount
  */
 class Order extends BaseModel
 {
@@ -115,6 +118,21 @@ class Order extends BaseModel
         }
 
         return $links;
+    }
+
+    public function getPreviousBalanceAttribute()
+    {
+        return $this->order_data['previous_balance'] ?? 0;
+    }
+
+    public function getCurrentBalanceAttribute()
+    {
+        return $this->order_data['current_balance'] ?? 0;
+    }
+
+    public function getTransactionAmountAttribute()
+    {
+        return $this->order_data['transaction_amount'] ?? 0;
     }
 
     /*
