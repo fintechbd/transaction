cf6d7d0c91e85c2860130589735a9339e1771565
hafijul233 <24684511+hafijul233@users.noreply.github.com>
2025-01-14 20:43:37 +0000

    Fix styling

--- src/Traits/HasOrderAttributes.php
+++ src/Traits/HasOrderAttributes.php
@@ -33,126 +33,126 @@ trait HasOrderAttributes
     public function amountFormatted(): Attribute
     {
         return Attribute::make(
-            get: fn(mixed $value, array $attributes) => (string)\currency($attributes['amount'], $attributes['currency'])
+            get: fn (mixed $value, array $attributes) => (string) \currency($attributes['amount'], $attributes['currency'])
         );
     }
 
     public function convertedAmountFormatted(): Attribute
     {
         return Attribute::make(
-            get: fn(mixed $value, array $attributes) => (string)\currency($attributes['converted_amount'], $attributes['converted_currency'])
+            get: fn (mixed $value, array $attributes) => (string) \currency($attributes['converted_amount'], $attributes['converted_currency'])
         );
     }
 
     public function chargeAmount(): Attribute
     {
         return Attribute::make(
-            get: fn(mixed $value, array $attributes) => $attributes['order_data']['service_stat_data']['charge_amount'] ?? null
+            get: fn (mixed $value, array $attributes) => $attributes['order_data']['service_stat_data']['charge_amount'] ?? null
         );
     }
 
     public function chargeAmountFormatted(): Attribute
     {
         return Attribute::make(
-            get: fn(mixed $value, array $attributes) => (string)\currency($attributes['order_data']['service_stat_data']['charge_amount'] ?? null, $attributes['currency'])
+            get: fn (mixed $value, array $attributes) => (string) \currency($attributes['order_data']['service_stat_data']['charge_amount'] ?? null, $attributes['currency'])
         );
     }
 
     public function discountAmount(): Attribute
     {
         return Attribute::make(
-            get: fn(mixed $value, array $attributes) => $attributes['order_data']['service_stat_data']['discount_amount'] ?? null
+            get: fn (mixed $value, array $attributes) => $attributes['order_data']['service_stat_data']['discount_amount'] ?? null
         );
     }
 
     public function discountAmountFormatted(): Attribute
     {
         return Attribute::make(
-            get: fn(mixed $value, array $attributes) => (string)\currency($attributes['order_data']['service_stat_data']['discount_amount'] ?? null, $attributes['currency'])
+            get: fn (mixed $value, array $attributes) => (string) \currency($attributes['order_data']['service_stat_data']['discount_amount'] ?? null, $attributes['currency'])
         );
     }
 
     public function commissionAmount(): Attribute
     {
         return Attribute::make(
-            get: fn(mixed $value, array $attributes) => $attributes['order_data']['service_stat_data']['commission_amount'] ?? null
+            get: fn (mixed $value, array $attributes) => $attributes['order_data']['service_stat_data']['commission_amount'] ?? null
         );
     }
 
     public function commissionAmountFormatted(): Attribute
     {
         return Attribute::make(
-            get: fn(mixed $value, array $attributes) => (string)\currency($attributes['order_data']['service_stat_data']['commission_amount'] ?? null, $attributes['currency'])
+            get: fn (mixed $value, array $attributes) => (string) \currency($attributes['order_data']['service_stat_data']['commission_amount'] ?? null, $attributes['currency'])
         );
     }
 
     public function costAmount(): Attribute
     {
         return Attribute::make(
-            get: fn(mixed $value, array $attributes) => $attributes['order_data']['service_stat_data']['cost_amount'] ?? null
+            get: fn (mixed $value, array $attributes) => $attributes['order_data']['service_stat_data']['cost_amount'] ?? null
         );
     }
 
     public function costAmountFormatted(): Attribute
     {
         return Attribute::make(
-            get: fn(mixed $value, array $attributes) => (string)\currency($attributes['order_data']['service_stat_data']['cost_amount'] ?? null, $attributes['currency'])
+            get: fn (mixed $value, array $attributes) => (string) \currency($attributes['order_data']['service_stat_data']['cost_amount'] ?? null, $attributes['currency'])
         );
     }
 
     public function totalAmount(): Attribute
     {
         return Attribute::make(
-            get: fn(mixed $value, array $attributes) => $attributes['order_data']['service_stat_data']['total_amount'] ?? null
+            get: fn (mixed $value, array $attributes) => $attributes['order_data']['service_stat_data']['total_amount'] ?? null
         );
     }
 
     public function totalAmountFormatted(): Attribute
     {
         return Attribute::make(
-            get: fn(mixed $value, array $attributes) => (string)\currency($attributes['order_data']['service_stat_data']['total_amount'] ?? null, $attributes['currency'])
+            get: fn (mixed $value, array $attributes) => (string) \currency($attributes['order_data']['service_stat_data']['total_amount'] ?? null, $attributes['currency'])
         );
     }
 
     public function interacCharge(): Attribute
     {
         return Attribute::make(
-            get: fn(mixed $value, array $attributes) => $attributes['order_data']['service_stat_data']['interac_charge_amount'] ?? null
+            get: fn (mixed $value, array $attributes) => $attributes['order_data']['service_stat_data']['interac_charge_amount'] ?? null
         );
     }
 
     public function interacChargeFormatted(): Attribute
     {
         return Attribute::make(
-            get: fn(mixed $value, array $attributes) => (string)\currency($attributes['order_data']['service_stat_data']['interac_charge_amount'] ?? null, $attributes['currency'])
+            get: fn (mixed $value, array $attributes) => (string) \currency($attributes['order_data']['service_stat_data']['interac_charge_amount'] ?? null, $attributes['currency'])
         );
     }
 
     public function previousAmount(): Attribute
     {
         return Attribute::make(
-            get: fn(mixed $value, array $attributes) => $attributes['order_data']['previous_amount'] ?? null
+            get: fn (mixed $value, array $attributes) => $attributes['order_data']['previous_amount'] ?? null
         );
     }
 
     public function previousAmountFormatted(): Attribute
     {
         return Attribute::make(
-            get: fn(mixed $value, array $attributes) => (string)\currency($attributes['order_data']['previous_amount'] ?? null, $attributes['currency'])
+            get: fn (mixed $value, array $attributes) => (string) \currency($attributes['order_data']['previous_amount'] ?? null, $attributes['currency'])
         );
     }
 
     public function currentAmount(): Attribute
     {
         return Attribute::make(
-            get: fn(mixed $value, array $attributes) => $attributes['order_data']['current_amount'] ?? null
+            get: fn (mixed $value, array $attributes) => $attributes['order_data']['current_amount'] ?? null
         );
     }
 
     public function currentAmountFormatted(): Attribute
     {
         return Attribute::make(
-            get: fn(mixed $value, array $attributes) => (string)\currency($attributes['order_data']['current_amount'] ?? null, $attributes['currency'])
+            get: fn (mixed $value, array $attributes) => (string) \currency($attributes['order_data']['current_amount'] ?? null, $attributes['currency'])
         );
     }
 
@@ -160,11 +160,11 @@ trait HasOrderAttributes
     {
         return [
             'currency' => $this->currency,
-            'amount' => (string)$this->amount,
+            'amount' => (string) $this->amount,
             'amount_formatted' => $this->amount_formatted,
 
             'converted_currency' => $this->converted_currency,
-            'converted_amount' => (string)$this->converted_amount,
+            'converted_amount' => (string) $this->converted_amount,
             'converted_amount_formatted' => $this->converted_amount_formatted,
 
             'charge_amount' => $this->charge_amount,
