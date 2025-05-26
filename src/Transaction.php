58c9a0d1f15190e3d93fec36df64be7e3173cbf1
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-05-26 17:22:52 +0600

    phpdoc block updated

--- src/Transaction.php
+++ src/Transaction.php
@@ -2,6 +2,7 @@
 
 namespace Fintech\Transaction;
 
+use Fintech\Core\Abstracts\BaseModel;
 use Fintech\Transaction\Services\ChartClassService;
 use Fintech\Transaction\Services\ChartEntryService;
 use Fintech\Transaction\Services\ChartTypeService;
@@ -16,74 +17,132 @@ use Fintech\Transaction\Services\RewardPointService;
 use Fintech\Transaction\Services\TransactionFormService;
 use Fintech\Transaction\Services\UserAccountService;
 use Fintech\Transaction\Support\Accounting;
+use Illuminate\Database\Eloquent\Collection;
 
 class Transaction
 {
+    /**
+     * @param $filters
+     * @return TransactionFormService|Collection|BaseModel
+     */
     public function transactionForm($filters = null)
     {
         return \singleton(TransactionFormService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return OrderService|Collection|BaseModel
+     */
     public function order($filters = null)
     {
         return \singleton(OrderService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return OrderDetailService|Collection|BaseModel
+     */
     public function orderDetail($filters = null)
     {
         return \singleton(OrderDetailService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return ChartClassService|Collection|BaseModel
+     */
     public function chartClass($filters = null)
     {
         return \singleton(ChartClassService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return ChartTypeService|Collection|BaseModel
+     */
     public function chartType($filters = null)
     {
         return \singleton(ChartTypeService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return ChartEntryService|Collection|BaseModel
+     */
     public function chartEntry($filters = null)
     {
         return \singleton(ChartEntryService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return UserAccountService|Collection|BaseModel
+     */
     public function userAccount($filters = null)
     {
         return \singleton(UserAccountService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return OrderQueueService|Collection|BaseModel
+     */
     public function orderQueue($filters = null)
     {
         return \singleton(OrderQueueService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return ManualRefundService|Collection|BaseModel
+     */
     public function manualRefund($filters = null)
     {
         return \singleton(ManualRefundService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return RewardPointService|Collection|BaseModel
+     */
     public function rewardPoint($filters = null)
     {
         return \singleton(RewardPointService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return RedeemPointService|Collection|BaseModel
+     */
     public function redeemPoint($filters = null)
     {
         return \singleton(RedeemPointService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return ComplianceService|Collection|BaseModel
+     */
     public function compliance($filters = null)
     {
         return \singleton(ComplianceService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return PolicyService|Collection|BaseModel
+     */
     public function policy($filters = null)
     {
         return \singleton(PolicyService::class, $filters);
     }
 
+    /**
+     * @param BaseModel $order
+     * @param string|int|null $user_id
+     * @return Accounting
+     */
     public function accounting(\Fintech\Core\Abstracts\BaseModel $order, string|int|null $user_id = null)
     {
         return new Accounting($order, $user_id);
